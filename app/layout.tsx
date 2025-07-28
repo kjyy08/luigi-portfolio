5e66e3464ece6a9b07955fbdc07dbd7b34bd4f1d
김주엽 <kjyy08@naver.com>
2025-07-28 23:50:02 +0900

    chore: favicon 파일 추가
    - meta 폴더에 favicon.svg 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,40 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Luigi's Backend Portfolio - 김주엽",
-  description: "무언가를 만들어가는 그 순간을 즐기는 백엔드 개발자 김주엽의 포트폴리오",
-    generator: 'v0.dev'
+  title: "Luigi's Portfolio",
+  description: "무언가를 만들어가는 그 순간을 즐기는 백엔드 개발자 김주엽입니다.",
+  generator: 'v0.dev',
+  icons: {
+    icon: [
+      { url: "/images/meta/favicon.ico" },
+      { url: "/images/meta/favicon.svg", type: "image/svg+xml" },
+      { url: "/images/meta/favicon-96x96.png", sizes: "96x96", type: "image/png" }
+    ],
+    apple: { url: "/images/meta/apple-touch-icon.png" },
+  },
+  manifest: "/images/meta/site.webmanifest",
+  openGraph: {
+    title: "Luigi's Portfolio",
+    description: "무언가를 만들어가는 그 순간을 즐기는 백엔드 개발자 김주엽입니다.",
+    type: "website",
+    locale: "ko_KR",
+    url: "https://luigi-portfolio.vercel.app/",
+    siteName: "Luigi's Portfolio",
+    images: [
+      {
+        url: "/images/meta/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Luigi's Portfolio"
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Luigi's Portfolio",
+    description: "무언가를 만들어가는 그 순간을 즐기는 백엔드 개발자 김주엽입니다.",
+    images: ["/images/meta/og.png"]
+  }
 }
 
 export default function RootLayout({
